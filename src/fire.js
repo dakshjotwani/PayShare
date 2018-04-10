dea2efe394b661882992bc171ae137332f677c09
MaxLin <maxylin@gmail.com>
2018-04-10 18:11:20 -0400

    Add firebase features

---
+++ src/fire.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAZ8is2W6ia0CoP0TC544ali49u1CkpZNg",
+  authDomain: "payshareapp.firebaseapp.com",
+  databaseURL: "https://payshareapp.firebaseio.com",
+  projectId: "payshareapp",
+  storageBucket: "payshareapp.appspot.com",
+  messagingSenderId: "445814351777"
+};
+firebase.initializeApp(config);
+export default firebase;
+// ... or you can use the equivalent shorthand notation
+// var defaultStorage = firebase.storage();
+// var defaultDatabase = firebase.database();
