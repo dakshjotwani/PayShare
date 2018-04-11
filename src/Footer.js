48837d7077b438397f72b5074ad2e3612cf56389
MaxLin <maxylin@gmail.com>
2018-04-10 22:49:40 -0400

    Add basic bootstrap navbar

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 // The Header creates links that can be used to navigate
 // between routes.
--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Nav, Navbar, NavItem } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 // The Header creates links that can be used to navigate
@@ -7,14 +8,30 @@ import { Link } from 'react-router-dom'
 class Header extends React.Component {
   render() {
     return (
-      <div className="App-header">
-        <nav>
-          <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/about'>About</Link></li>
-          </ul>
-        </nav>
-      </div>
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to='/'>PayShare</Link>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <NavItem componentClass={Link} eventKey={1} href="/" to="/">
+            Home
+    </NavItem>
+          <NavItem componentClass={Link} eventKey={2} href="/about" to="/about">
+            About
+    </NavItem>
+    {/* 
+          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+            <MenuItem eventKey={3.1}>Action</MenuItem>
+            <MenuItem eventKey={3.2}>Another action</MenuItem>
+            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={3.4}>Separated link</MenuItem>
+          </NavDropdown>
+    */}
+        </Nav>
+      </Navbar>
     );
   }
 }
