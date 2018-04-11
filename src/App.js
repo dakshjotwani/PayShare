a144a799a54f325dc1ca8a1c6f9a284b45fbc66d
MaxLin <maxylin@gmail.com>
2018-04-10 22:00:36 -0400

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
 import firebase from './fire';
 import logo from './logo.svg';
+import Header from './Header'
+import Footer from './Footer'
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
