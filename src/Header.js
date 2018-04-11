b6629f0f274ae6e108ad4b916ac86b0c5f1fceee
MaxLin <maxylin@gmail.com>
2018-04-10 23:25:57 -0400

    Add static name dropdown

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Nav, Navbar, NavItem } from 'react-bootstrap'
+import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 // The Header creates links that can be used to navigate
 // between routes.
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    // TODO: Switch to using react context
+    this.state = { name: "Name" }
+  }
+
   render() {
     return (
       <Navbar>
@@ -21,15 +27,15 @@ class Header extends React.Component {
           <NavItem componentClass={Link} eventKey={2} href="/about" to="/about">
             About
     </NavItem>
-    {/* 
-          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+        </Nav>
+        <Nav pullRight>
+          <NavDropdown eventKey={3} title={this.state.name} id="basic-nav-dropdown">
             <MenuItem eventKey={3.1}>Action</MenuItem>
             <MenuItem eventKey={3.2}>Another action</MenuItem>
             <MenuItem eventKey={3.3}>Something else here</MenuItem>
             <MenuItem divider />
             <MenuItem eventKey={3.4}>Separated link</MenuItem>
           </NavDropdown>
-    */}
         </Nav>
       </Navbar>
     );
