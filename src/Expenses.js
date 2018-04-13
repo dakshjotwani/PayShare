67d84b39c48fe158e2e9452d115782e1bc6d60d1
MaxLin <maxylin@gmail.com>
2018-04-13 17:32:23 -0400

    Add Expenses file

---
+++ src/Expenses.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import {
+} from 'reactstrap';
+
+// The Header creates links that can be used to navigate
+// between routes.
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    // TODO: Switch to using react context
+    this.state = { name: "Name" }
+  }
+
+  render() {
+  }
+}
+
+export default Expenses;
