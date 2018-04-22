fb94b0aa7670c9075742d6fb9ea2425417fd5598
Daksh Jotwani <dakshjotwani@gmail.com>
2018-04-22 02:34:00 -0400

    Add firebase auth

--- src/SignIn.js
+++ src/SignIn.js
@@ -4,19 +4,22 @@ import {firebase, auth} from './fire';
 
 
 class SignIn extends React.Component {
-    state = {
-        isSignedIn: false
-    };
-    
-    uiConfig = {
-        signInFlow: 'popup',
-        signInSuccessUrl: '/expenses',
-        signInOptions: [
-            firebase.auth.EmailAuthProvider.PROVIDER_ID, 
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        ]
-    };
-  
+    constructor(props) {
+        super(props);
+        this.state = {
+            authed: this.props.authed
+        };
+        this.uiConfig = {
+            signInFlow: 'popup',
+            signInSuccessUrl: '/expenses',
+            signInOptions: [
+                firebase.auth.EmailAuthProvider.PROVIDER_ID, 
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            ]
+        };
+    }
+
+        /*
     componentDidMount() {
         this.unregisterAuthObserver = auth.onAuthStateChanged(
             (user) => this.setState({isSignedIn: !!user})
@@ -25,20 +28,21 @@ class SignIn extends React.Component {
 
     componentWillUnmount() {
         this.unregisterAuthObserver();
-    }
+    } */
 
     render() {
-        if (!this.state.isSignedIn) {
+        if (!this.state.authed) {
             return (
                 <div>
+                    <h1> Sign in </h1>
                     <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={auth}/>
                 </div>
             )
         }
-        
+
         return (
             <div>
-                
+                <h1> You are already signed in! </h1>
             </div>
         )
     }
