dac51ddeaad28cb96e40999f5a0e2f53467e43d7
Daksh Jotwani <dakshjotwani@gmail.com>
2018-06-01 17:13:42 +0530

    Looks better, functions better. Consider changing reactstrap Tooltip to reactstrap Popover

--- src/Item.js
+++ src/Item.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import { ListGroupItem, UncontrolledTooltip, Button } from 'reactstrap';
-// Props:
-// key = key
-// item = this.state.items[key]
-// currentUser = auth.currentUser.uid
-// onClick = this.handleChange
-// onClickRemove = this.handleRemoveItem
+import {
+    ListGroupItem, UncontrolledTooltip,
+    Button, Badge
+} from 'reactstrap';
 
 class Item extends React.Component {
     render() {
@@ -24,32 +21,59 @@ class Item extends React.Component {
                 }
                 key={this.props.item.index}
                 name={this.props.item.index}
-                onClick={this.props.onClick}
-                action
             >
                 <div className="row justify-content-between">
                     <div className="col-6">
-                        {this.props.item.name}
+                        <Button
+                            color="link"
+                            key={this.props.item.index}
+                            name={this.props.item.index}
+                            onClick={this.props.onClick}
+                            style={{whiteSpace: 'normal',
+                                color: 'black',
+                                textDecoration: 'none'
+                            }}
+                        >
+                            {this.props.item.name}
+                        </Button>
                     </div>
-                    <div className="col-2">
+                    <div className="col-1">
                         <Button id={"usersTooltip-" + this.props.id} color="link">
-                            <i className="fas fa-users"></i>
+                            <Badge color="secondary" pill>{names.length}</Badge>
                         </Button>
                         <UncontrolledTooltip
                             placement="top"
                             target={"usersTooltip-" + this.props.id}
                         >
-                            {names.join()}
+                            {names.join() !== ""
+                                ? names.join(", ")
+                                : "Nobody has selected this item."}
                         </UncontrolledTooltip>
                     </div>
                     <div className="col-2">
-                        {parseFloat(this.props.item.price).toFixed(2)}
+                        <Button
+                            color="link"
+                            key={this.props.item.index}
+                            name={this.props.item.index}
+                            onClick={this.props.onClick}
+                            style={{whiteSpace: 'normal',
+                                color: 'black',
+                                textDecoration: 'none'
+                            }}
+                        >
+                            {parseFloat(this.props.item.price).toFixed(2)}
+                        </Button>
                     </div>
                     <div className="col-2">
                         <Button
-                            color="danger"
-                            size="sm"
-                            style={{float: 'right'}}
+                            color="link"
+                            style={{
+                                float: 'right',
+                                textDecoration: 'none',
+                                color: this.props.item.users.hasOwnProperty(
+                                    this.props.currentUser
+                                ) ? "green" : "red"
+                            }}
                             name={this.props.item.index}
                             onClick={this.props.onClickRemove}
                         >
