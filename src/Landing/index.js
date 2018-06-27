f29da76afdf3304642131eba079cd8b206ef43d5
vishwakalyanaraman <vishwa.kalyanaraman@gmail.com>
2018-06-27 17:02:41 +0530

    Fix syntax issues (ESLint)

--- src/Landing/index.js
+++ src/Landing/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Jumbotron, Button, Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle,} from 'reactstrap';
+  CardTitle, CardSubtitle} from 'reactstrap';
 import './index.css';
 import {Link} from 'react-router-dom';
 
@@ -13,53 +13,59 @@ export default class Landing extends React.Component {
      * @return {object} Landing page
      */
     render() {
-	return (
+    return (
             <div>
                 <Jumbotron fluid className="head">
                     <h1 className="display-3">PayShare</h1>
                     <p className="lead">
-		    The easy way to split expenses.
+            The easy way to split expenses.
                     </p>
                     <p className="lead1">
                         <Button color="primary" size="lg" tag={Link}
-			to="/signin"><strong>Get Started &#10132;</strong></Button>
+            to="/signin"><strong>Get Started &#10132;</strong></Button>
                     </p>
                 </Jumbotron>
 
-		<div className="text-center" id="aboutus">
-		    <h3 id="info-title">Split Bills Easily</h3>
-		    <p className="text-left" id="info-text">
-		    You go out a lot. You shouldn't waste your time deciding who pays for what and
-		    how much. You deserve PayShare so we made it for you. Enjoy!
-		    </p>
+        <div className="text-center" id="aboutus">
+            <h3 id="info-title">Split Bills Easily</h3>
+            <p className="text-left" id="info-text">
+            You go out a lot. You shouldn&apos;t waste your time deciding who p
+            ays for what and how much. You deserve PayShare so we made it for y
+            ou. Enjoy!
+            </p>
 
-		    <h3 id="info-title1">Track Your Expenses</h3>
-		    <p className="text-right" id="info-text1">
-		    Get an overview on how much you've spent going out to eat. You can quickly scan
-		    through your expenses or get an in depth overview on individual spendings.
-		    </p>
-		</div>
+            <h3 id="info-title1">Track Your Expenses</h3>
+            <p className="text-right" id="info-text1">
+            Get an overview on how much you&apos;ve spent going out to eat. You
+            can quickly scan through your expenses or get an in depth overview
+            on individual spendings.
+            </p>
+        </div>
 
-		<div id="developers">
-			 <Card id="leftcard">
-        			<CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-        			<CardBody>
-          				<CardTitle>Card title</CardTitle>
-          				<CardSubtitle>Card subtitle</CardSubtitle>
-          				<CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-        			</CardBody>
-			</Card>
- 
-			 <Card id="rightcard">
-        			<CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-        			<CardBody>
-          				<CardTitle>Card title</CardTitle>
-          				<CardSubtitle>Card subtitle</CardSubtitle>
-          				<CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-        			</CardBody>
-			</Card> 
-		</div>
-	    </div>
+        <div id="developers">
+            <Card id="leftcard">
+                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>Card title</CardTitle>
+                        <CardSubtitle>Card subtitle</CardSubtitle>
+                        <CardText>Some quick example text to build on the card
+                        title and make up the bulk of the card&apos;s content.
+                        </CardText>
+                    </CardBody>
+            </Card>
+
+             <Card id="rightcard">
+                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>Card title</CardTitle>
+                        <CardSubtitle>Card subtitle</CardSubtitle>
+                        <CardText>Some quick example text to build on the card
+                        title and make up the bulk of the card&apos;s content.
+                         </CardText>
+                    </CardBody>
+            </Card>
+        </div>
+        </div>
         );
-    }
+     }
 }
